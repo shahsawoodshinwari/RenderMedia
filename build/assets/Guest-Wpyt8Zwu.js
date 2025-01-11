423ad818bf891428e44d86d9725681f255b5a0d9
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-01-11 18:57:55 +0500

    chore: latest fix

---
+++ build/assets/Guest-Wpyt8Zwu.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,a as n,o as c,r as s}from"./index-BthXMaJw.js";const r={},a={class:"container d-flex flex-column vh-100 justify-content-center align-items-center"};function _(i,l){const e=s("RouterView");return c(),o("div",a,[n(e)])}const m=t(r,[["render",_]]);export{m as default};
