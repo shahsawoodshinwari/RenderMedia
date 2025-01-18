450f8df218bf5f99be648861153069f39a13fcbf
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-01-18 14:53:37 +0500

    chore: removed hardcode values

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -4,8 +4,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    // 'X-API-Key': import.meta.env.VITE_API_KEY,
-    'X-API-Key': `eyJpdiI6IjBxZEdST1BscWlVdVRnNWZXT05TM2c9PSIsInZhbHVlIjoiMXJMdzZmMEg4dGZaNjdUTm1mYVZpUzNBdDdjT0NWUW5wd1JIVU9DbzlFbz0iLCJtYWMiOiJiYWVlYTU1NTc3Mjg2ZmYwYmMwNDY1ZWRmYzQ4ZTY1NGIyNDkzNTVkNDBmMzBmZjhhMWY0MDJhNTc2MmUwM2JkIiwidGFnIjoiIn0=`,
+    'X-API-Key': import.meta.env.VITE_API_KEY,
   },
   withCredentials: false,
 })
@@ -14,7 +13,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('authToken')
     if (token) {
-      config.headers.Authorization = `Bearer 111|JSYo6XREsPhIsvK9ZmYe7JOe5nTopKIO4OM8dMLW44a2e27c`
+      config.headers.Authorization = token
     }
     return config
   },
