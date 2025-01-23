b6455f4ae18315e56c16f1851735e2fc1a7d6c6f
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-01-23 15:28:08 +0500

    Updates

---
+++ assets/FilmingLocationView--5yFurWF.js
@@ -0,0 +1,1 @@
+import{_ as e,s as a,c as n,t as o,o as r}from"./index-CGuEuORG.js";const c={name:"FilmingLocationView",data(){return{payload:{lat:null,lng:null,date:null,time:null}}},props:{category:{type:String,required:!0}},provide(){return{category:a(()=>this.category)}}};function i(l,s,t,u,p,d){return r(),n("div",null,o(t.category),1)}const m=e(c,[["render",i]]);export{m as default};
--- assets/FilmingLocationView-Biui7sxa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,s as a,c as n,t as o,o as r}from"./index-C-IaE0sQ.js";const c={name:"FilmingLocationView",data(){return{payload:{lat:null,lng:null,date:null,time:null}}},props:{category:{type:String,required:!0}},provide(){return{category:a(()=>this.category)}}};function i(l,s,t,u,p,d){return r(),n("div",null,o(t.category),1)}const m=e(c,[["render",i]]);export{m as default};
