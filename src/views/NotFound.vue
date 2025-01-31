e855e1dd74feb5c82e87da992a66f20e7026aa05
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-01-31 20:06:28 +0500

    chore: fixed 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,20 +1,20 @@
 <script>
+import Image from '@/assets/404.webp'
+
 export default {
-  mounted() {
-    const link = document.createElement('link')
-    link.rel = 'preload'
-    link.href = '../assets/404.webp'
-    link.as = 'image'
-    document.head.appendChild(link)
+  computed: {
+    image() {
+      return Image;
+    },
   },
-}
+};
 </script>
 <template>
   <div class="container text-center py-5">
     <h1 class="display-1 text-danger">404</h1>
     <p class="lead text-muted">Page Not Found</p>
 
-    <img src="../assets/404.webp" class="img-fluid w-75 mb-4 rounded" alt="404 Not Found" />
+    <img :src="image" class="img-fluid w-75 mb-4 rounded" alt="404 Not Found" />
 
     <p class="text-muted">
       The resource you are looking for could not be found on the server. It might have been removed
@@ -22,7 +22,7 @@ export default {
     </p>
 
     <!-- Suggest returning to home -->
-    <router-link to="/" class="btn btn-primary"> Go to Home </router-link>
+    <RouterLink to="/" class="btn btn-primary"> Go to Home </RouterLink>
   </div>
 </template>
 
